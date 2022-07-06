9168226d4b20e7599b237d9761d5367fa93ef31b
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-06 15:40:24 -0500

    accept user data

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -90,7 +88,6 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -98,7 +95,6 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
